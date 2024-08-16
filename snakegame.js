b9a29d8b06d38f0959e450d1b606e511ae5a2503
Htetyinsoe <htetyysoe@gmail.com>
2024-08-15 22:42:33 -0700

    maked the snake move.

--- snakegame.js
+++ snakegame.js
@@ -1,7 +1,14 @@
 const gridSize = 20;
 const board = document.getElementById("game-board");
+const instructionText = document.getElementById("instruction-text");
+const logo = document.getElementById("logo");
+
 let snake = [{ x: 10, y: 10 }];
 let food = generateFood();
+let direction = "right";
+let gameIntervel;
+let gameSpeedDelay = 200;
+let gameStarted = false;
 
 function draw() {
   board.innerHTML = "";
@@ -39,4 +46,45 @@ function generateFood() {
   const y = Math.floor(Math.random() * gridSize) + 1;
   return { x, y };
 }
-draw();
+
+function move() {
+  const head = { ...snake[0] };
+  switch (direction) {
+    case "up":
+      head.y--;
+      break;
+    case "right":
+      head.x++;
+      break;
+    case "down":
+      head.y++;
+      break;
+    case "left":
+      head.x--;
+      break;
+  }
+  snake.unshift(head);
+  //snake.pop();
+
+  if (head.x === food.x && head.y === food.y) {
+    generateFood();
+    clearInterval();
+    gameIntervel = setInterval(() => {
+      move();
+      draw();
+    }, gameSpeedDelay);
+  } else {
+    snake.pop();
+  }
+}
+
+function startGame() {
+  gameStarted = true;
+  instructionText.style.display = "none";
+  logo.style.display = "none";
+}
+
+//setInterval(() => {
+//move();
+//draw();
+//}, 200);
